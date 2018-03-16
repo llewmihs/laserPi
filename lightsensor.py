d912f4724ecc6e6af684f0ac9a188504f70f0797
Joe Shimwell <spgk4@c15000173.campus.unn.ac.uk>
2018-03-16 14:01:25 +0000

    Test

--- lightsensor.py
+++ lightsensor.py
@@ -1,7 +1,40 @@
-from gpiozero import LightSensor
-sensor = LightSensor(18)
-from time import sleep
+#!/usr/bin/env python
+
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+
+import RPi.GPIO as GPIO, time, os
+from gpiozero import LED
+
+laser = LED(14)
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+
+def RCtime (RCpin):
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
+
+    GPIO.setup(RCpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
+    while (GPIO.input(RCpin) == GPIO.LOW):
+        reading += 1
+    return reading
+
+#start up - get average light reading with and without LED
+# laser.off()
+# total=0
+# for i in range(5):
+#     total = total + RCtime(18)
+#     time.sleep(0.2)
+# avDark = total/5
+# print("Average dark reading is %d" % avDark)
 
 while True:
-    print(sensor.value)
-    sleep(0.2)
+    #turn of the LED
+    #laser.off()
+    print(RCtime(18))     # Read RC timing using pin #18
+    #get average light level
+
