77d3b1c9f942f9d747b594f13c51274e509e03ed
Joe Shimwell <spgk4@c15000173.campus.unn.ac.uk>
2018-03-16 14:32:14 +0000

    oisg

--- laser.py
+++ laser.py
@@ -9,10 +9,10 @@ delay = 0.25
 while True:
     Voltage1  = explorerhat.analog.four.read()
     print('Voltage is %d' % Voltage1)
-    if V2 > threshold:
-        explorerhat.output.one.off()
-        explorerhat.output.two.on()
-    else:
-        explorerhat.output.one.on()
-        explorerhat.output.two.off()        
+    # if V2 > threshold:
+    #     explorerhat.output.one.off()
+    #     explorerhat.output.two.on()
+    # else:
+    #     explorerhat.output.one.on()
+    #     explorerhat.output.two.off()        
     time.sleep(delay)
