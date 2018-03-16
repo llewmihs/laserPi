67b7ecc5e9724b986cb71b4f00145267f6435603
Joe Shimwell <spgk4@c15000173.campus.unn.ac.uk>
2018-03-16 14:32:56 +0000

    sdf

--- laser.py
+++ laser.py
@@ -8,7 +8,7 @@ delay = 0.25
 
 while True:
     Voltage1  = explorerhat.analog.four.read()
-    print('Voltage is %d' % Voltage1)
+    print('Voltage is %f' % Voltage1)
     # if V2 > threshold:
     #     explorerhat.output.one.off()
     #     explorerhat.output.two.on()
